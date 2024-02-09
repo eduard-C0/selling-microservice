aa1f1a2943050a073e733b79a941e7ecf3dd3674
Eduard Cosma <eduardcosma2000@gmail.com>
2024-02-09 14:01:01 +0200

    refactored

--- src/adoption/selling.controller.ts
+++ src/adoption/selling.controller.ts
@@ -2,11 +2,11 @@ import { Controller } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
 import { SellingService } from './selling.service';
 
-@Controller('selling')
+@Controller()
 export class SellingController {
     constructor(private sellingService: SellingService){}
 
-    @EventPattern("adopt_animal")
+    @EventPattern("sell_car")
     async handlerAnimalAdoption(carId: number){
         this.sellingService.updateCarSelling(Number(carId))
     }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SellingModule } from './adoption/selling.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [SellingModule, PrismaModule],
+  imports: [SellingModule, PrismaModule, ConfigModule.forRoot({isGlobal: true})],
   controllers: [AppController],
   providers: [AppService],
 })
